749a52e4f5b719503e583840348d486ad288bcd9
Narendra Chinnakotla <narendrachinnakotla13@gmail.com>
2025-12-15 07:34:45 +0530

    git initial commit for TwoSum problem solution in Java

---
+++ ArrayProblems/TwoSum.java
@@ -0,0 +1,39 @@
+package ArrayProblems;
+
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        // Optimized approach using a hash map
+        Map<Integer, Integer> numMap = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[] {-1, -1};
+
+        // Brute-force approach
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }
+        // return null; // Return null if no solution is found
+    }
+}
